3b0340cccbadfa8b41ac6f14fb7dc1f7f3571a65
Roope Mäkinen <noby992@gmail.com>
2020-10-13 23:07:40 +0300

    Implemented FIR lowpass filter for oversampling in the Corrosion device. Needs much optimization, this is just a proff of concept.

--- WaveSabreCore/src/Corrosion.cpp
+++ WaveSabreCore/src/Corrosion.cpp
@@ -1,154 +1,244 @@
 #include <WaveSabreCore/Corrosion.h>
 #include <WaveSabreCore/Helpers.h>
 
-#include <math.h>
+#ifdef _DEBUG
+#include <cassert>
+#include <cmath>
+#endif
 
 namespace WaveSabreCore
 {
-	Corrosion::Corrosion()
-		: Device((int)ParamIndices::NumParams)
+	Corrosion::Corrosion() :
+		Device((int)ParamIndices::NumParams),
+		inputGain(0.0f),
+		twist(0.0f),
+		saturation(0.0f),
+		oversampling(Oversampling::X1),
+		dryWet(1.0f)
 	{
-		inputGain = 0.0f;
-		twist = 0.0f;
-		saturation = 0.0f;
-		oversampling = Oversampling::X1;
-		dryWet = 1.0f;
+		createSincImpulse(firResponse2, Taps2, 0.243);
+		createSincImpulse(firResponse4, Taps4, 0.243*0.5);
+	}
 
-		const float nyquist2 = (Helpers::CurrentSampleRate * 0.5f) / 2.0f;
-		const float Q2 = 0.0f;
-		lowpassUpsample2[0].Set(nyquist2, Q2);
-		lowpassUpsample2[1].Set(nyquist2, Q2);
-		lowpassDownsample2[0].Set(nyquist2, Q2);
-		lowpassDownsample2[1].Set(nyquist2, Q2);
+	void Corrosion::createSincImpulse(float* result, const int taps, const double cutoff)
+	{
+		const double Pi = 3.141592653589793;
+		const double M = static_cast<double>(taps) - 1.0;
 
-		const float nyquist4 = (Helpers::CurrentSampleRate * 0.5f) / 4.0f;
-		const float Q4 = 0.0f;
-		lowpassUpsample4[0].Set(nyquist4, Q4);
-		lowpassUpsample4[1].Set(nyquist4, Q4);
-		lowpassDownsample4[0].Set(nyquist4, Q4);
-		lowpassDownsample4[1].Set(nyquist4, Q4);
+		// Generate sinc impulse with a window.
+		for (int i = 0; i < taps; ++i) {
+			const double d = static_cast<double>(i);
+			double impulse = 0.0;
+			if (d == M*0.5) {
+				impulse = 2.0*cutoff;
+			}
+			else {
+				double n = d - M*0.5;
+				impulse = Helpers::FastSin(2.0*Pi*cutoff*n) / (Pi*n);
+			}
+
+			// Blackman window.
+			const double window = 0.42 - (0.5*Helpers::FastCos(2.0*Pi*d/M)) + (0.08*Helpers::FastCos(4.0*Pi*d/M));
+			result[i] = static_cast<float>(impulse * window);
+		}
+
+#ifdef _DEBUG
+		float sum = 0.0f;
+		for(int i = 0; i < taps; ++i) sum += result[i];
+		assert(fabsf(1.0f - sum) < 0.001f);
+#endif
 	}
 
 	void Corrosion::Run(double songPosition, float **inputs, float **outputs, int numSamples)
 	{
+		for(int i = 0; i < 2; ++i)
+		{
+			for(int j = 0; j < currentBufferLength; ++j)
+				pastBuffer[i][j] = currentBuffer[i][j];
+			for(int j = 0; j < futureBufferLength; ++j)
+				currentBuffer[i][j] = futureBuffer[i][j];
+			for(int j = 0; j < numSamples; ++j)
+				futureBuffer[i][j] = inputs[i][j];
+		}
+
+		pastBufferLength = currentBufferLength;
+		currentBufferLength = futureBufferLength;
+		futureBufferLength = numSamples;
+
+		if(pastBufferLength < Taps2>>1) return;
+		
+		float inputGainScalar = Helpers::DbToScalar(inputGain);
 		const float param1 = 10.0f * (twist * twist);
 		const float param2 = 20.0f * saturation;
 
-		float inputGainScalar = Helpers::DbToScalar(inputGain);
-
-		for (int i = 0; i < 2; i++)
+		if(oversampling == Oversampling::X1)
 		{
-			for (int j = 0; j < numSamples; j++)
+			for(int i = 0; i < 2; ++i)
 			{
-				const float input = inputs[i][j];
-				const float inputWithGain = input * inputGainScalar;
-				
-				switch (oversampling)
+				for(int j = 0; j < currentBufferLength; ++j)
 				{
-				case Oversampling::X1:
-					{
-						float v = shape(inputWithGain, param1, param2);
-						outputs[i][j] = Helpers::Mix(input, v, dryWet);
-					}
-					break;
-				case Oversampling::X2:
-					{
-						// Insert zeros and lowpass.
-						// Filtered signal requires gain due to the zeros.
-#if 0
-						buffer[i][j*2]   = lowpassUpsample2[i].Next((lastSample[i]+inputWithGain)*0.5f);
-						buffer[i][j*2+1] = lowpassUpsample2[i].Next(inputWithGain);
-#else
-						buffer[i][j*2]   = 2.0f*lowpassUpsample2[i].Next(inputWithGain);
-						buffer[i][j*2+1] = 2.0f*lowpassUpsample2[i].Next(0.0f);
-						
-#endif
-						// Oversample waveshaping.
-						buffer[i][j*2]   = shape(buffer[i][j*2], param1, param2);
-						buffer[i][j*2+1] = shape(buffer[i][j*2+1], param1, param2);
-
-						// Lowpass result.
-						buffer[i][j*2]   = lowpassDownsample2[i].Next(buffer[i][j*2]);
-						buffer[i][j*2+1] = lowpassDownsample2[i].Next(buffer[i][j*2+1]);
-					}
-					break;
-
-				case Oversampling::X4:
-					{
-						// Insert zeros and lowpass.
-						// Filtered signal requires gain due to the zeros.
-#if 0
-						float inputMid = (lastSample[i]+inputWithGain)*0.5f;
-						float inputQ1 = (lastSample[i]+inputMid)*0.5f;
-						float inputQ2 = (inputMid+inputWithGain)*0.5f;
-						buffer[i][j*4]   = lowpassUpsample4[i].Next(inputQ1);
-						buffer[i][j*4+1] = lowpassUpsample4[i].Next(inputMid);
-						buffer[i][j*4+2] = lowpassUpsample4[i].Next(inputQ2);
-						buffer[i][j*4+3] = lowpassUpsample4[i].Next(inputWithGain);
-#else
-						buffer[i][j*4]   = 4.0f*lowpassUpsample4[i].Next(inputWithGain);
-						buffer[i][j*4+1] = 4.0f*lowpassUpsample4[i].Next(0.0f);
-						buffer[i][j*4+2] = 4.0f*lowpassUpsample4[i].Next(0.0f);
-						buffer[i][j*4+3] = 4.0f*lowpassUpsample4[i].Next(0.0f);
-#endif
-
-						// Oversample waveshaping.
-						buffer[i][j*4]   = shape(buffer[i][j*4], param1, param2);
-						buffer[i][j*4+1] = shape(buffer[i][j*4+1], param1, param2);
-						buffer[i][j*4+2] = shape(buffer[i][j*4+2], param1, param2);
-						buffer[i][j*4+3] = shape(buffer[i][j*4+3], param1, param2);
-
-						// Lowpass result.
-						buffer[i][j*4]   = lowpassDownsample4[i].Next(buffer[i][j*4]);
-						buffer[i][j*4+1] = lowpassDownsample4[i].Next(buffer[i][j*4+1]);
-						buffer[i][j*4+2] = lowpassDownsample4[i].Next(buffer[i][j*4+2]);
-						buffer[i][j*4+3] = lowpassDownsample4[i].Next(buffer[i][j*4+3]);
-					}
-					break;
+					const float input = currentBuffer[i][j];
+					const float inputWithGain = input * inputGainScalar;
+					float v = shape(inputWithGain, param1, param2);
+					outputs[i][j] = Helpers::Mix(input, v, dryWet);
 				}
 			}
 		}
-
-#ifndef CHEAP_OVERSAMPLING
-		// Decimate oversampled result.
-		if(oversampling == Oversampling::X2)
+		else if(oversampling == Oversampling::X2)
 		{
-			for (int i = 0; i < 2; i++)
+#ifdef TEST_FIR
+			for(int j = 0; j < pastBufferLength; ++j)
 			{
-				for (int j = 0; j < numSamples; j++)
+				oversamplingBuffer[i][j]   = pastBuffer[i][j];
+			}
+			for(int j = 0; j < currentBufferLength; ++j)
+			{
+				oversamplingBuffer[i][pastBufferLength + j]   = currentBuffer[i][j];
+			}
+			for(int j = 0; j < futureBufferLength; ++j)
+			{
+				oversamplingBuffer[i][pastBufferLength+currentBufferLength+j]   = futureBuffer[i][j];
+			}
+
+			for(int j = 0; j < currentBufferLength; ++j)
+			{
+				float filteredSample = 0.0f;
+				for(int k = 0; k < Taps; ++k)
 				{
-#ifdef AVERAGE
-					outputs[i][j] = Helpers::Mix(inputs[i][j], (buffer[i][j*2]+buffer[i][j*2+1])*0.5f, dryWet);
+					int cs = j + (k - ht);
+					float sample = oversamplingBuffer[i][currentBufferLength+cs];
+					filteredSample += sample * firResponse[k];
+				}
+
+				outputs[i][j] = filteredSample;
+			}
 #else
-					outputs[i][j] = Helpers::Mix(inputs[i][j], buffer[i][j*2+1], dryWet);
-#endif
+			const int HalfTaps = Taps2>>1;
+			for(int i = 0; i < 2; ++i)
+			{
+				// Make a buffer for oversampling, insert zeros between samples.
+				for(int j = 0; j < pastBufferLength; ++j)
+				{
+					oversamplingBuffer[i][j*2]   = pastBuffer[i][j];
+					oversamplingBuffer[i][j*2+1] = 0.0f;
+				}
+				for(int j = 0; j < currentBufferLength; ++j)
+				{
+					oversamplingBuffer[i][(pastBufferLength + j)*2]   = currentBuffer[i][j];
+					oversamplingBuffer[i][(pastBufferLength + j)*2+1] = 0.0f;
+				}
+				for(int j = 0; j < futureBufferLength; ++j)
+				{
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*2]   = futureBuffer[i][j];
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*2+1] = 0.0f;
+				}
+				
+				// Filter above original nyquist and waveshape.
+				for(int j = -HalfTaps; j < currentBufferLength*2 + HalfTaps; ++j)
+				{
+					float filteredSample = 0.0f;
+					for(int k = 0; k < Taps2; ++k)
+					{
+						int cs = j + (k - HalfTaps);
+						float sample = oversamplingBuffer[i][currentBufferLength*2+cs];
+						filteredSample += sample * firResponse2[k];
+					}
+
+					waveshapingBuffer[i][j+HalfTaps] = shape(2.0f*inputGainScalar*filteredSample, param1, param2);
+				}
+
+				// Band limit to original nyquist
+				for(int j = 0; j < currentBufferLength*2; ++j)
+				{
+					float bandlimitedSample = 0.0f;
+					for(int k = 0; k < Taps2; ++k)
+					{
+						int cs = j + (k - HalfTaps);
+						float sample = waveshapingBuffer[i][HalfTaps+cs];
+						bandlimitedSample += sample * firResponse2[k];
+					}
+
+					bandlimitingBuffer[i][j] = bandlimitedSample;
+				}
+
+				// Decimate and hope for the best??
+				for(int j = 0; j  < numSamples; ++j)
+				{
+					outputs[i][j] = Helpers::Mix(currentBuffer[i][j], bandlimitingBuffer[i][j*2], dryWet);
 				}
 			}
+#endif
 		}
 		else if(oversampling == Oversampling::X4)
 		{
-			for (int i = 0; i < 2; i++)
+			const int HalfTaps = Taps4>>1;
+			for(int i = 0; i < 2; ++i)
 			{
-				for (int j = 0; j < numSamples; j++)
+				// Make a buffer for oversampling, insert zeros between samples.
+				for(int j = 0; j < pastBufferLength; ++j)
 				{
-#ifdef AVERAGE
-					outputs[i][j] = Helpers::Mix(inputs[i][j], (buffer[i][j*4]+buffer[i][j*4+1]+buffer[i][j*4+2]+buffer[i][j*4+3])*0.25f, dryWet);
-#else
-					// Don't know which sample is correct to decimate...
-					// the third sample seems to have the best characteristics.
-					outputs[i][j] = Helpers::Mix(inputs[i][j], buffer[i][j*4+2], dryWet);
-#endif
+					oversamplingBuffer[i][j*4]   = pastBuffer[i][j];
+					oversamplingBuffer[i][j*4+1] = 0.0f;
+					oversamplingBuffer[i][j*4+2] = 0.0f;
+					oversamplingBuffer[i][j*4+3] = 0.0f;
+				}
+				for(int j = 0; j < currentBufferLength; ++j)
+				{
+					oversamplingBuffer[i][(pastBufferLength + j)*4]   = currentBuffer[i][j];
+					oversamplingBuffer[i][(pastBufferLength + j)*4+1] = 0.0f;
+					oversamplingBuffer[i][(pastBufferLength + j)*4+2] = 0.0f;
+					oversamplingBuffer[i][(pastBufferLength + j)*4+3] = 0.0f;
+				}
+				for(int j = 0; j < futureBufferLength; ++j)
+				{
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*4]   = futureBuffer[i][j];
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*4+1] = 0.0f;
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*4+2] = 0.0f;
+					oversamplingBuffer[i][(pastBufferLength+currentBufferLength+j)*4+3] = 0.0f;
+				}
+				
+				// Filter above original nyquist and waveshape.
+				for(int j = -HalfTaps; j < currentBufferLength*4 + HalfTaps; ++j)
+				{
+					float filteredSample = 0.0f;
+					for(int k = 0; k < Taps4; ++k)
+					{
+						int cs = j + (k - HalfTaps);
+						float sample = oversamplingBuffer[i][currentBufferLength*4+cs];
+						filteredSample += sample * firResponse4[k];
+					}
+
+					waveshapingBuffer[i][j+HalfTaps] = shape(4.0f*inputGainScalar*filteredSample, param1, param2);
+				}
+
+				// Band limit to original nyquist
+				for(int j = 0; j < currentBufferLength*4; ++j)
+				{
+					float bandlimitedSample = 0.0f;
+					for(int k = 0; k < Taps4; ++k)
+					{
+						int cs = j + (k - HalfTaps);
+						float sample = waveshapingBuffer[i][HalfTaps+cs];
+						bandlimitedSample += sample * firResponse4[k];
+					}
+
+					bandlimitingBuffer[i][j] = bandlimitedSample;
+				}
+
+				// Decimate and hope for the best??
+				for(int j = 0; j  < numSamples; ++j)
+				{
+					outputs[i][j] = Helpers::Mix(currentBuffer[i][j], bandlimitingBuffer[i][j*4+3], dryWet);
 				}
 			}
 		}
-#endif
 	}
 
 	float Corrosion::shape(float input, float p1, float p2)
 	{
 		// Apply sine function wave shaping.
 		const float twist = Helpers::Mix(input,
-										 Helpers::FastSin(static_cast<float>(input * Helpers::Mix(1.0f, 2.0f * 3.141592f, p1))),
+										 static_cast<float>(Helpers::FastSin(input * Helpers::Mix(1.0f, 2.0f * 3.141592f, p1))),
 										 Helpers::Clamp(0.0f, 1.0f, p1));
 		
 		// Apply tanh function wave shaping.
