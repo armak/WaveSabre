acc3bd16b8b03ba6cf3effa5267b7d2a03e6cc8a
Roope Mäkinen <noby992@gmail.com>
2021-05-11 03:01:34 +0300

    Changed the oversampling selection to a switch statement.

--- WaveSabreCore/src/Corrosion.cpp
+++ WaveSabreCore/src/Corrosion.cpp
@@ -62,146 +62,152 @@ namespace WaveSabreCore
 		const float param3 = 100.0f * (fold * fold);
 		const float param4 =  20.0f * saturation;
 
-		if(oversampling == Oversampling::X1)
-		{
-			for(int i = 0; i < 2; ++i)
-			{
-				for(int j = 0; j < numSamples; ++j)
-				{
-					const float input = inputs[i][j];
-					const float inputWithGain = input * inputGainScalar;
-					float v = shape(inputWithGain, param1, param2, param3, param4);
-					outputs[i][j] = Helpers::Mix(input, v, dryWet);
-				}
-			}
-
-			return;
-		}
-
 		// TODO: Everything will fail horribly if numSamples is less than width of the FIR kernel,
 		// i.e. ASIO latency lower than 64 samples for 2x, or lower than 128 samples for 4x.
 
-		if(oversampling == Oversampling::X2)
+		switch(oversampling)
 		{
-			const int HalfTaps = Taps2>>1;
-			for(int i = 0; i < 2; ++i)
+			case Oversampling::X1:
 			{
-				for(int j = 0; j < HalfTaps; ++j)
+				for(int i = 0; i < 2; ++i)
 				{
-					dryBuffer[i][j] = previousBuffer[i][j + HalfTaps];
-				}
-				for(int j = 0; j < Taps2; ++j)
-				{
-					oversamplingBuffer[i][j*2]   = previousBuffer[i][j];
-					oversamplingBuffer[i][j*2+1] = 0.0f;
-				}
-				for(int j = 0; j < numSamples; ++j)
-				{
-					dryBuffer[i][HalfTaps + j] = inputs[i][j];
-
-					oversamplingBuffer[i][(Taps2 + j)*2]   = inputs[i][j];
-					oversamplingBuffer[i][(Taps2 + j)*2+1] = 0.0f;
-				}
-
-				// Filter above original nyquist and waveshape.
-				for(int j = 0; j < numSamples*2 + Taps2; ++j)
-				{
-					float filteredSample = 0.0f;
-					for(int k = 0; k < Taps2; ++k)
+					for(int j = 0; j < numSamples; ++j)
 					{
-						float sample = oversamplingBuffer[i][j + k];
-						filteredSample += sample * firResponse2[k];
+						const float input = inputs[i][j];
+						const float inputWithGain = input * inputGainScalar;
+						float v = shape(inputWithGain, param1, param2, param3, param4);
+						outputs[i][j] = Helpers::Mix(input, v, dryWet);
 					}
-
-					waveshapingBuffer[i][j] = shape(2.0f*inputGainScalar*filteredSample, param1, param2, param3, param4);
 				}
 
-				// Band limit to original nyquist.
-				for(int j = 0; j < numSamples*2; ++j)
-				{
-					float bandlimitedSample = 0.0f;
-					for(int k = 0; k < Taps2; ++k)
-					{
-						float sample = waveshapingBuffer[i][j + k];
-						bandlimitedSample += sample * firResponse2[k];
-					}
-
-					bandlimitingBuffer[i][j] = bandlimitedSample;
-				}
-
-				// Decimate the bandlimited signal for output.
-				for(int j = 0; j  < numSamples; ++j)
-				{
-					outputs[i][j] = Helpers::Mix(dryBuffer[i][j], bandlimitingBuffer[i][j*2], dryWet);
-				}
-
-				for(int j = 0; j < Taps2; ++j)
-				{
-					previousBuffer[i][j] = inputs[i][numSamples - Taps2 + j];
-				}
+				break;
 			}
-		}
-		else if(oversampling == Oversampling::X4)
-		{
-			const int HalfTaps = Taps4>>1;
-			for(int i = 0; i < 2; ++i)
+
+			case Oversampling::X2:
 			{
-				for(int j = 0; j < HalfTaps; ++j)
+				const int HalfTaps = Taps2>>1;
+				for(int i = 0; i < 2; ++i)
 				{
-					dryBuffer[i][j] = previousBuffer[i][j + HalfTaps];
-				}
-				for(int j = 0; j < Taps4; ++j)
-				{
-					oversamplingBuffer[i][j*4]   = previousBuffer[i][j];
-					oversamplingBuffer[i][j*4+1] = 0.0f;
-					oversamplingBuffer[i][j*4+2] = 0.0f;
-					oversamplingBuffer[i][j*4+3] = 0.0f;
-				}
-				for(int j = 0; j < numSamples; ++j)
-				{
-					dryBuffer[i][HalfTaps + j] = inputs[i][j];
-
-					oversamplingBuffer[i][(Taps4 + j)*4]   = inputs[i][j];
-					oversamplingBuffer[i][(Taps4 + j)*4+1] = 0.0f;
-					oversamplingBuffer[i][(Taps4 + j)*4+2] = 0.0f;
-					oversamplingBuffer[i][(Taps4 + j)*4+3] = 0.0f;
-				}
-
-				// Filter above original nyquist and waveshape.
-				for(int j = 0; j < numSamples*4 + Taps4*2; ++j)
-				{
-					float filteredSample = 0.0f;
-					for(int k = 0; k < Taps4; ++k)
+					for(int j = 0; j < HalfTaps; ++j)
 					{
-						float sample = oversamplingBuffer[i][j + k];
-						filteredSample += sample * firResponse4[k];
+						dryBuffer[i][j] = previousBuffer[i][j + HalfTaps];
+					}
+					for(int j = 0; j < Taps2; ++j)
+					{
+						oversamplingBuffer[i][j*2]   = previousBuffer[i][j];
+						oversamplingBuffer[i][j*2+1] = 0.0f;
+					}
+					for(int j = 0; j < numSamples; ++j)
+					{
+						dryBuffer[i][HalfTaps + j] = inputs[i][j];
+
+						oversamplingBuffer[i][(Taps2 + j)*2]   = inputs[i][j];
+						oversamplingBuffer[i][(Taps2 + j)*2+1] = 0.0f;
 					}
 
-					waveshapingBuffer[i][j] = shape(4.0f*inputGainScalar*filteredSample, param1, param2, param3, param4);
-				}
-				
-				// Band limit to original nyquist.
-				for(int j = 0; j < numSamples*4 + Taps4; ++j)
-				{
-					float bandlimitedSample = 0.0f;
-					for(int k = 0; k < Taps4; ++k)
+					// Filter above original nyquist and waveshape.
+					for(int j = 0; j < numSamples*2 + Taps2; ++j)
 					{
-						float sample = waveshapingBuffer[i][j + k];
-						bandlimitedSample += sample * firResponse4[k];
+						float filteredSample = 0.0f;
+						for(int k = 0; k < Taps2; ++k)
+						{
+							float sample = oversamplingBuffer[i][j + k];
+							filteredSample += sample * firResponse2[k];
+						}
+
+						waveshapingBuffer[i][j] = shape(2.0f*inputGainScalar*filteredSample, param1, param2, param3, param4);
 					}
 
-					bandlimitingBuffer[i][j] = bandlimitedSample;
-				}
-				
-				// Decimate the bandlimited signal for output.
-				for(int j = 0; j  < numSamples; ++j)
-				{
-					outputs[i][j] = Helpers::Mix(dryBuffer[i][j], bandlimitingBuffer[i][j*4 + Taps4], dryWet);
+					// Band limit to original nyquist.
+					for(int j = 0; j < numSamples*2; ++j)
+					{
+						float bandlimitedSample = 0.0f;
+						for(int k = 0; k < Taps2; ++k)
+						{
+							float sample = waveshapingBuffer[i][j + k];
+							bandlimitedSample += sample * firResponse2[k];
+						}
+
+						bandlimitingBuffer[i][j] = bandlimitedSample;
+					}
+
+					// Decimate the bandlimited signal for output.
+					for(int j = 0; j  < numSamples; ++j)
+					{
+						outputs[i][j] = Helpers::Mix(dryBuffer[i][j], bandlimitingBuffer[i][j*2], dryWet);
+					}
+
+					for(int j = 0; j < Taps2; ++j)
+					{
+						previousBuffer[i][j] = inputs[i][numSamples - Taps2 + j];
+					}
 				}
 
-				for(int j = 0; j < Taps4; ++j)
+				break;
+			}
+
+			case Oversampling::X4:
+			{
+				const int HalfTaps = Taps4>>1;
+				for(int i = 0; i < 2; ++i)
 				{
-					previousBuffer[i][j] = inputs[i][numSamples - Taps4 + j];
+					for(int j = 0; j < HalfTaps; ++j)
+					{
+						dryBuffer[i][j] = previousBuffer[i][j + HalfTaps];
+					}
+					for(int j = 0; j < Taps4; ++j)
+					{
+						oversamplingBuffer[i][j*4]   = previousBuffer[i][j];
+						oversamplingBuffer[i][j*4+1] = 0.0f;
+						oversamplingBuffer[i][j*4+2] = 0.0f;
+						oversamplingBuffer[i][j*4+3] = 0.0f;
+					}
+					for(int j = 0; j < numSamples; ++j)
+					{
+						dryBuffer[i][HalfTaps + j] = inputs[i][j];
+
+						oversamplingBuffer[i][(Taps4 + j)*4]   = inputs[i][j];
+						oversamplingBuffer[i][(Taps4 + j)*4+1] = 0.0f;
+						oversamplingBuffer[i][(Taps4 + j)*4+2] = 0.0f;
+						oversamplingBuffer[i][(Taps4 + j)*4+3] = 0.0f;
+					}
+
+					// Filter above original nyquist and waveshape.
+					for(int j = 0; j < numSamples*4 + Taps4*2; ++j)
+					{
+						float filteredSample = 0.0f;
+						for(int k = 0; k < Taps4; ++k)
+						{
+							float sample = oversamplingBuffer[i][j + k];
+							filteredSample += sample * firResponse4[k];
+						}
+
+						waveshapingBuffer[i][j] = shape(4.0f*inputGainScalar*filteredSample, param1, param2, param3, param4);
+					}
+				
+					// Band limit to original nyquist.
+					for(int j = 0; j < numSamples*4 + Taps4; ++j)
+					{
+						float bandlimitedSample = 0.0f;
+						for(int k = 0; k < Taps4; ++k)
+						{
+							float sample = waveshapingBuffer[i][j + k];
+							bandlimitedSample += sample * firResponse4[k];
+						}
+
+						bandlimitingBuffer[i][j] = bandlimitedSample;
+					}
+				
+					// Decimate the bandlimited signal for output.
+					for(int j = 0; j  < numSamples; ++j)
+					{
+						outputs[i][j] = Helpers::Mix(dryBuffer[i][j], bandlimitingBuffer[i][j*4 + Taps4], dryWet);
+					}
+
+					for(int j = 0; j < Taps4; ++j)
+					{
+						previousBuffer[i][j] = inputs[i][numSamples - Taps4 + j];
+					}
 				}
 			}
 		}
