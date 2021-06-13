1cab547d86a07069a5abe1617ec63a126609d9ed
Roope Mäkinen <noby992@gmail.com>
2021-06-13 03:22:56 +0300

    Adjust multiband parameters, change butterworth back to floats and optimize a bit.

--- WaveSabreCore/src/Multiband.cpp
+++ WaveSabreCore/src/Multiband.cpp
@@ -86,25 +86,25 @@ namespace WaveSabreCore
 			case Multiband::ParamIndices::MidBandCutoff: crossovers[1].setCutoff(Helpers::ParamToFrequency(value)); break;
 			case Multiband::ParamIndices::HighBandCutoff: crossovers[2].setCutoff(Helpers::ParamToFrequency(value)); break;
 
-			case Multiband::ParamIndices::Band1Threshold: bands[0].threshold = Helpers::ParamToDb(value / 2.0f, 36.0f); break;
+			case Multiband::ParamIndices::Band1Threshold: bands[0].threshold = Helpers::ParamToDb(value / 2.0f, 48.0f); break;
 			case Multiband::ParamIndices::Band1Ratio: bands[0].ratio = value*value*18.0f + 2.0f; break;
 			case Multiband::ParamIndices::Band1Attack: bands[0].attack = Helpers::ScalarToEnvValue(value) / 5.0f; break;
 			case Multiband::ParamIndices::Band1Release: bands[0].release = Helpers::ScalarToEnvValue(value); break;
 			case Multiband::ParamIndices::Band1Gain: bands[0].gain = Helpers::ParamToDb(value, 12.0f); break;
 
-			case Multiband::ParamIndices::Band2Threshold: bands[1].threshold = Helpers::ParamToDb(value / 2.0f, 36.0f); break;
+			case Multiband::ParamIndices::Band2Threshold: bands[1].threshold = Helpers::ParamToDb(value / 2.0f, 48.0f); break;
 			case Multiband::ParamIndices::Band2Ratio: bands[1].ratio = value*value*18.0f + 2.0f; break;
 			case Multiband::ParamIndices::Band2Attack: bands[1].attack = Helpers::ScalarToEnvValue(value) / 5.0f; break;
 			case Multiband::ParamIndices::Band2Release: bands[1].release = Helpers::ScalarToEnvValue(value); break;
 			case Multiband::ParamIndices::Band2Gain: bands[1].gain = Helpers::ParamToDb(value, 12.0f); break;
 
-			case Multiband::ParamIndices::Band3Threshold: bands[2].threshold = Helpers::ParamToDb(value / 2.0f, 36.0f); break;
+			case Multiband::ParamIndices::Band3Threshold: bands[2].threshold = Helpers::ParamToDb(value / 2.0f, 48.0f); break;
 			case Multiband::ParamIndices::Band3Ratio: bands[2].ratio = value*value*18.0f + 2.0f; break;
 			case Multiband::ParamIndices::Band3Attack: bands[2].attack = Helpers::ScalarToEnvValue(value) / 5.0f; break;
 			case Multiband::ParamIndices::Band3Release: bands[2].release = Helpers::ScalarToEnvValue(value); break;
 			case Multiband::ParamIndices::Band3Gain: bands[2].gain = Helpers::ParamToDb(value, 12.0f); break;
 
-			case Multiband::ParamIndices::Band4Threshold: bands[3].threshold = Helpers::ParamToDb(value / 2.0f, 36.0f); break;
+			case Multiband::ParamIndices::Band4Threshold: bands[3].threshold = Helpers::ParamToDb(value / 2.0f, 48.0f); break;
 			case Multiband::ParamIndices::Band4Ratio: bands[3].ratio = value*value*18.0f + 2.0f; break;
 			case Multiband::ParamIndices::Band4Attack: bands[3].attack = Helpers::ScalarToEnvValue(value) / 5.0f; break;
 			case Multiband::ParamIndices::Band4Release: bands[3].release = Helpers::ScalarToEnvValue(value); break;
@@ -126,25 +126,25 @@ namespace WaveSabreCore
 			case Multiband::ParamIndices::MidBandCutoff: return Helpers::FrequencyToParam(crossovers[1].cutoff); break;
 			case Multiband::ParamIndices::HighBandCutoff: return Helpers::FrequencyToParam(crossovers[2].cutoff); break;
 
-			case Multiband::ParamIndices::Band1Threshold: return Helpers::DbToParam(bands[0].threshold, 36.0f) * 2.0f; break;
+			case Multiband::ParamIndices::Band1Threshold: return Helpers::DbToParam(bands[0].threshold, 48.0f) * 2.0f; break;
 			case Multiband::ParamIndices::Band1Ratio: return sqrtf((bands[0].ratio - 2.0f) / 18.0f); break;
 			case Multiband::ParamIndices::Band1Attack: return Helpers::EnvValueToScalar(bands[0].attack*5.0f); break;
 			case Multiband::ParamIndices::Band1Release: return Helpers::EnvValueToScalar(bands[0].release); break;
 			case Multiband::ParamIndices::Band1Gain: return Helpers::DbToParam(bands[0].gain, 12.0f); break;
 
-			case Multiband::ParamIndices::Band2Threshold: return Helpers::DbToParam(bands[1].threshold, 36.0f) * 2.0f; break;
+			case Multiband::ParamIndices::Band2Threshold: return Helpers::DbToParam(bands[1].threshold, 48.0f) * 2.0f; break;
 			case Multiband::ParamIndices::Band2Ratio: return sqrtf((bands[1].ratio - 2.0f) / 18.0f); break;
 			case Multiband::ParamIndices::Band2Attack: return Helpers::EnvValueToScalar(bands[1].attack*5.0f); break;
 			case Multiband::ParamIndices::Band2Release: return Helpers::EnvValueToScalar(bands[1].release); break;
 			case Multiband::ParamIndices::Band2Gain: return Helpers::DbToParam(bands[1].gain, 12.0f); break;
 
-			case Multiband::ParamIndices::Band3Threshold: return Helpers::DbToParam(bands[2].threshold, 36.0f) * 2.0f; break;
+			case Multiband::ParamIndices::Band3Threshold: return Helpers::DbToParam(bands[2].threshold, 48.0f) * 2.0f; break;
 			case Multiband::ParamIndices::Band3Ratio: return sqrtf((bands[2].ratio - 2.0f) / 18.0f); break;
 			case Multiband::ParamIndices::Band3Attack: return Helpers::EnvValueToScalar(bands[2].attack*5.0f); break;
 			case Multiband::ParamIndices::Band3Release: return Helpers::EnvValueToScalar(bands[2].release); break;
 			case Multiband::ParamIndices::Band3Gain: return Helpers::DbToParam(bands[2].gain, 12.0f); break;
 
-			case Multiband::ParamIndices::Band4Threshold: return Helpers::DbToParam(bands[3].threshold, 36.0f) * 2.0f; break;
+			case Multiband::ParamIndices::Band4Threshold: return Helpers::DbToParam(bands[3].threshold, 48.0f) * 2.0f; break;
 			case Multiband::ParamIndices::Band4Ratio: return sqrtf((bands[3].ratio - 2.0f) / 18.0f); break;
 			case Multiband::ParamIndices::Band4Attack: return Helpers::EnvValueToScalar(bands[3].attack*5.0f); break;
 			case Multiband::ParamIndices::Band4Release: return Helpers::EnvValueToScalar(bands[3].release); break;
